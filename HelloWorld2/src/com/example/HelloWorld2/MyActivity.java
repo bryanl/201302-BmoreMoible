edbd302fae90bf1ab88c193eec9f2464c55603cd
bryanl <bryan@osesm.com>
2013-02-20 14:54:27 -0500

    a hello world sample

---
+++ HelloWorld2/src/com/example/HelloWorld2/MyActivity.java
@@ -0,0 +1,31 @@
+package com.example.HelloWorld2;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        final Context thisActivity = this;
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(thisActivity, "You pressed me", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+
+}
---
+++ HelloWorld2/src/com/example/HelloWorld2/SuperCrazyButton.java
@@ -0,0 +1,26 @@
+package com.example.HelloWorld2;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+public class SuperCrazyButton extends Button {
+    public SuperCrazyButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public SuperCrazyButton(Context context) {
+        super(context);
+        init();
+    }
+
+    public SuperCrazyButton(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    private void init() {
+        setBackgroundResource(R.drawable.button_background);
+    }
+}
